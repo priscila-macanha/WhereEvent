d101055fe1e8ce19797032a86337fdd9e9c95cb5
priscila <priscila@bvcdigital.com.br>
2020-07-05 15:47:29 -0300

    Initial Commit

---
+++ WhereEvent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereEvent
+//
+//  Created by Priscila on 05/07/20.
+//  Copyright © 2020 Priscila Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhereEventTests/WhereEventTests.swift
@@ -0,0 +1,34 @@
+//
+//  WhereEventTests.swift
+//  WhereEventTests
+//
+//  Created by Priscila on 05/07/20.
+//  Copyright © 2020 Priscila Alves. All rights reserved.
+//
+
+import XCTest
+@testable import WhereEvent
+
+class WhereEventTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
